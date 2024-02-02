e895fd343a5f55f1b6f11fa9125abde0f9434345
zhufucdev <1095291434@qq.com>
2024-02-02 08:24:26 +0800

    :memo: Documentation for Box, EmptyBox and BlockBox

--- stub/src/main/java/com/zhufucdev/me/stub/Box.kt
+++ stub/src/main/java/com/zhufucdev/me/stub/Box.kt
@@ -3,6 +3,9 @@ package com.zhufucdev.me.stub
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.serializer
 
+/**
+ * Wraps something for it to be possibly empty or blocked ([Motion] and [CellTimeline])
+ */
 open class Box<T>(val value: T? = null) {
     open val status: Toggle
         get() = Toggle.PRESENT
@@ -27,27 +30,47 @@ open class Box<T>(val value: T? = null) {
     }
 }
 
+/**
+ * Something that bypasses to the original method
+ */
 class EmptyBox<T> : Box<T>() {
     override val status: Toggle
         get() = Toggle.NONE
 }
 
+/**
+ * Something that can but doesn't have a value
+ */
 class BlockBox<T> : Box<T>() {
     override val status: Toggle
         get() = Toggle.BLOCK
 }
 
+/**
+ * Status of a [Box]
+ */
 enum class Toggle {
     BLOCK, NONE, PRESENT
 }
 
+/**
+ * Wraps something into a [Box]
+ */
 fun <T> T.box(): Box<T> = Box(this)
+
+/**
+ * Wraps something into a [Box] unless it's null, and in this case
+ * wraps to [EmptyBox]
+ */
 fun <T> T.boxOrEmpty(): Box<T> = this?.let { Box(it) } ?: EmptyBox()
 
 const val EMPTY_REF = "none"
 const val BLOCK_REF = "block"
 const val NULL_REF = "null"
 
+/**
+ * Encode a [Box] into [Json] with its [Box.value], or an [EmptyBox] into [EMPTY_REF], or a [BlockBox] into [BLOCK_REF]
+ */
 inline fun <reified T> Box<T>.encodeToString(): String =
     when (this) {
         is EmptyBox -> EMPTY_REF
@@ -55,11 +78,12 @@ inline fun <reified T> Box<T>.encodeToString(): String =
         else -> value?.let { Json.encodeToString(serializer<T>(), it) } ?: "null"
     }
 
-
-class MutableBox<T>(var value: T)
-
-fun <T> T.mutbox() = MutableBox(this)
-
+/**
+ * Get its reference if a [Box]'s [Box.value] is a [Data]
+ * - [EmptyBox]<[Data]> returns [EMPTY_REF]
+ * - [BlockBox]<[Data]> returns [BLOCK_REF]
+ * - [Box]<[Data]> returns [Box.value]->[Data.id]
+ */
 fun <T : Data> Box<T>.ref() =
     when (this) {
         is EmptyBox<T> -> EMPTY_REF
