5d0e47af52caadbd3c29d3295205b90c292c1190
zhufucdev <1095291434@qq.com>
2024-02-02 10:09:07 +0800

    :memo: Documentation for MutableBox

--- stub/src/main/java/com/zhufucdev/me/stub/MutableBox.kt
+++ stub/src/main/java/com/zhufucdev/me/stub/MutableBox.kt
@@ -1,5 +1,11 @@
 package com.zhufucdev.me.stub
 
+/**
+ * Wraps a variable
+ */
 class MutableBox<T>(var value: T)
 
+/**
+ * Wraps a variable and returns its [MutableBox]
+ */
 fun <T> T.mutbox() = MutableBox(this)
